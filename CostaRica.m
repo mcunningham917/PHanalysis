966b2c3c9128ee53529c02107e9a320c44901948
Maxwell Cunningham <cunningham.mt917@gmail.com>
2019-01-09 14:18:50 -0500

    Changed axes limits for CR, FN

--- CostaRica.m
+++ CostaRica.m
@@ -6,7 +6,8 @@ addpath(phToolsPath)
 %% Define job parameters, include Region, supercatchment
 
 groupArea = 'CostaRica';
-supercatchmentNum = [1];
+supercatchmentNum = [1:36];
 outputFigType = 'png';
+peakElevationForOutputFig = 4000;
 %%
 RunPH
--- Finisterres.m
+++ Finisterres.m
@@ -5,7 +5,7 @@ addpath(phToolsPath)
 
 %% Define job parameters, include Region, supercatchment
 
-groupArea = 'Merida';
+groupArea = 'Finisterres';
 supercatchmentNum = [1:39];
 outputFigType = 'png';
 peakElevationForOutputFig = 4500;
